20612895947e129282e08529b05cc0cad01e169a
NIKIT <nikita2008-101@yandex.ru>
2016-12-19 04:21:46 +0300

    )

--- ConsoleApplication7/ConsoleApplication7.cpp
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -43,16 +43,16 @@ struct newspapers {
 	}
 	newspapers(int index__, std::string name__, std::string fioofred__, double_t cost__, std::string surname__) {
 		index = index__;
-		name = name__;
+		name = name__; 
 		fioofred = fioofred__;
 		cost = cost__;
 		surname = surname__;
 	}
 	int index =0;//индекс издания
-	std::string name ="";//название газеты
-	std::string fioofred = "";//ФИО редактора
+	std::string name ="-";//название газеты
+	std::string fioofred = "-";//ФИО редактора
 	double_t cost = 0;//цена газеты
-	std::string surname = "";//фамилия подписчика
+	std::string surname = "-";//фамилия подписчика
 	std::string to_string() {
 		return (std::to_string(index) + " | " + name +" | "+ fioofred + " | " + std::to_string(cost) + " | " + surname);
 	}
@@ -141,7 +141,6 @@ public:
 	void Check();
 private:
 	MemberMultiset baseset;
-	std::vector<MyClass> values;
 };
 
 MainManager::MainManager()
@@ -193,20 +192,18 @@ void MainManager::ReadAsFile() {
 			std::cout << (sArr[INDEX].c_str()) << '\n';
 			try
 			{
-				values.push_back(
-					MyClass(
+				MyClass * newNode = new MyClass(
+					std::atoi(sArr[INDEX].c_str()),
+					rand(),
+					newspapers(
 						std::atoi(sArr[INDEX].c_str()),
-						rand(),
-						newspapers(
-							std::atoi(sArr[INDEX].c_str()),
-							sArr[NAME],
-							sArr[FIOOFRED],
-							std::atof(sArr[COST].c_str()),
-							sArr[SURNAME])
-					)
+						std::to_string(std::atoi(sArr[NAME].c_str())),
+						sArr[FIOOFRED],
+						std::atof(sArr[COST].c_str()),
+						sArr[SURNAME])
 				);
-				baseset.insert(values.back());
-				std::cout << values.back().get_newspaper().to_string() << '\n';
+				baseset.insert(*newNode);
+				std::cout << (*newNode).get_newspaper().to_string() << '\n';
 			}
 			catch (const std::exception&)
 			{
@@ -252,27 +249,26 @@ void MainManager::Find()
 		std::cout << "ERROR" << '\n';
 	}
 }
-void MainManager::Check() {	
-}
 void MainManager::Add() {
 	std::string line;
 	std::cout << "Add:" << '\n';
 	std::getline(std::cin, line);
-	std::string* sArr = split(line, ";");
+	std::string* sArr = new std::string[100];
+	sArr = split(line, ";");
 	try
 	{
-		values.push_back(MyClass(
+		MyClass * newNode = new MyClass(
 			std::atoi(sArr[INDEX].c_str()),
 			rand(),
 			newspapers(
 				std::atoi(sArr[INDEX].c_str()),
-				sArr[NAME],
+				std::to_string(std::atoi(sArr[NAME].c_str())),
 				sArr[FIOOFRED],
 				std::atof(sArr[COST].c_str()),
 				sArr[SURNAME])
-		));
-		baseset.insert(values.back());
-		std::cout << (sArr[INDEX].c_str()) << "ADD_NORNAL\n "<< values.back().get_newspaper().to_string() << '\n';
+		);
+		baseset.insert(*newNode);
+		std::cout << (sArr[INDEX].c_str()) << "ADD_NORNAL\n "<< '\n';
 	}
 	catch (const std::exception&)
 	{
@@ -283,58 +279,27 @@ void MainManager::Add() {
 MainManager::~MainManager()
 {
 }
-
+void MainManager::Check() {
+	for (int i = 0; i < 10; ++i) {
+		MyClass * m = new MyClass(
+			i,
+			rand(),
+			newspapers(
+				i,
+				std::to_string(i)+"txt",
+				std::to_string(i)+"txt",
+				i,
+				"const txt")
+		);
+		baseset.insert(*m);
+	}
+	for (MemberMultiset::iterator it(baseset.begin()), itend(baseset.end()); it != itend; ++it) {
+		std::cout << (*it).get_newspaper().to_string() << '\n';
+	}
+}
 int main()
 {	
 	MainManager* mainManager = new MainManager();
-	return 0; 
-	//Там начинается лютая жесть, лучше не смотреть.
-	/*
-	typedef std::vector<MyClass>::iterator VectIt;
-	std::vector<MyClass> values;
-
-	MemberMultiset membermultiset;
-
-	for (int i = 0; i < 100; ++i) {
-	newspapers newspaper(i, (std::string)("112345"+ std::to_string(i)));
-	values.push_back(MyClass(i, (i % 10), newspaper));
-	}
-	//Now insert them in the sets
-	for (VectIt it(values.begin()), itend(values.end()); it != itend; ++it) {
-		baseset.insert(*it);
-		membermultiset.insert(*it);
-		//newspapers newspaper(1, (std::string)("x11234567"));
-		//auto qw = new MyClass(2, (2 % 10), newspaper);
-		
-		//std::printf("%s", baseset.find(*it)->newspaper_.name.c_str());
-		//std::printf("%s", it->newspaper_.name.c_str());
-	}
-	for (int i = 0; i < 100; ++i) {
-		std::printf("%s \n", (*baseset.find(MyClass(i))).get_newspaper().to_string());
-	}
-	//Now test treap_sets
-	{
-		BaseSet::reverse_iterator rbit(baseset.rbegin()), rbitend(baseset.rend());
-		MemberMultiset::iterator mit(membermultiset.begin()), mitend(membermultiset.end());
-		VectIt it(values.begin()), itend(values.end());
-
-		//Test the objects inserted in the base hook treap_set
-		for (; it != itend; ++it, ++rbit)
-			if (&*rbit != &*it)   return 1;
-
-		//Test the objects inserted in the member hook treap_set
-		for (it = values.begin(); it != itend; ++it, ++mit)
-			if (&*mit != &*it) return 1;
-
-		//Test priority order
-		for (int i = 0; i < 100; ++i) {
-			if (baseset.top()->get_priority() != static_cast<unsigned int>(i / 10))
-				return 1;
-			if (membermultiset.top()->get_priority() != 9u - static_cast<unsigned int>(i / 10))
-				return 1;
-			baseset.erase(baseset.top());
-			membermultiset.erase(membermultiset.top());
-		}
-	}*/
+	return 0;
 }
 
